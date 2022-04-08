86f4d9eb8c58514ab26c4c8602eeac81db75055e
qkrwhddms <qkrwhddms01@gmail.com>
2022-04-08 09:35:33 +0900

    stu

---
+++ study/code02-05.py
@@ -0,0 +1,15 @@
+#code02-05.py
+
+import turtle
+
+turtle.shape('turtle')
+
+turtle.forward(200)
+turtle.right(90)
+turtle.forward(200)
+turtle.right(90)
+turtle.forward(200)
+turtle.right(90)
+turtle.forward(200)
+
+turtle.done()
